890c819f89c7dd6e860d229860271de7e4f258dd
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-14 17:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   var objectClone = Object.assign({}, object, {[key]:value})
    return objectClone;
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[[key]] = value
+  return object
+}
